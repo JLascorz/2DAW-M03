e698eeb0407c29959268bc02d63dc3625d72f666
JLascorz <jlascorzsmx@gmail.com>
2016-10-03 19:25:35 +0200

    Commit del hello_dev2

--- hello_world.java
+++ hello_world.java
@@ -3,6 +3,6 @@ package jlascorz.exercici2;
 public class Application {
 
 	public static void main(String[] args) {
-		system.out.println("Hello World: To forgive all is as inhuman as to forgive none (Aristòtil). Fortunately we have GIT");
+		system.out.println("Hello World: To forgive all is as inhuman as to forgive none (Seneca). Fortunately we have GIT");
 	}
 }
