f67d06a34be3f7b46e6b8c17c69f7f7d79bc11b6
JLascorz <jlascorzsmx@gmail.com>
2016-10-03 20:04:44 +0200

    Commit desde Hello World Normal

--- hello_world.java
+++ hello_world.java
@@ -3,6 +3,6 @@ package jlascorz.exercici2;
 public class Application {
 
 	public static void main(String[] args) {
-		system.out.println("Hello World!");
+		system.out.println("Hello World From Vilanova");
 	}
 }
