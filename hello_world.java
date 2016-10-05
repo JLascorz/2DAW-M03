f4470f1de80da71752c9352e60950c9beef7023f
JLascorz <jlascorzsmx@gmail.com>
2016-10-05 16:32:15 +0200

    Original

--- hello_world.java
+++ hello_world.java
@@ -4,7 +4,7 @@ public class Application {
 
 	public static void main(String[] args) {
 
-		system.out.println("Jelou Uol From Vilanova To forgive all is as inhuman as to forgive none (Seneca). Fortunately we have GIT");
+		system.out.println("Hello World!!");
 
 	}
 }
